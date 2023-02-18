c1b6c3386b2ff3b3c1f1dfd7b085e4e40747316e
Nelson Dane <danenb@clarkson.edu>
2023-02-18 14:26:07 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# GCalSwitcher
+# GCalSwitcher for Tampermonkey
  
+A small TamperMonkey script that automatically switches to a different Google Calendar Account. 
+
+For example, I use the Clarkson University calendar on my Clarkson account so I can see classes and assignments. It gets annoying when I load Google Calendar and it loads a different account, so this script will automatically switch to the Clarkson account when I load Google Calendar.
+
+## Installation
+
+1. Install [Tampermonkey](https://www.tampermonkey.net/) for your browser.
+2. Open the raw script file [here](https://raw.githubusercontent.com/NelsonDane/GCalSwitcher/main/gcal.js).
+3. Open the Tampermonkey dashboard and click the "Create a new script..." button.
+4. Paste the contents of the raw script file into the new script.
---
+++ gcal.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Clarkson GCal Switcher
+// @namespace    https://calendar.google.com/
+// @version      1.0
+// @description  Reloads until "Clarkson University" in page title
+// @author       NelsonDane
+// @match        https://calendar.google.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @run-at       document-idle
+// ==/UserScript==
+
+'use strict'
+
+// String to search for in page title
+const SEARCH = "Clarkson University";
+
+// Maximum number of accounts to check
+const MAX = 5;
+
+// Get previous count value, setting to 0 if not found
+var count = await GM.getValue("count", 0);
+if (count >= MAX) {
+    await GM.setValue("count", 0);
+    count = 0;
+}
+console.log("Count: " + count);
+
+async function reload(count, SEARCH) {
+    // If found, we're done, so reset count to 0
+    if (document.title.includes(SEARCH)) {
+        console.log("Found " + SEARCH + " in page title");
+        await GM.setValue("count", 0);
+    } else {
+        // Otherwise, reload with next account
+        console.log("Did not find " + SEARCH + " in page title")
+        await GM.setValue("count", count + 1);
+        window.location.href = "https://calendar.google.com/calendar/u/" + count + "/r";
+    }
+}
+
+// Run reload
+reload(count, SEARCH);
